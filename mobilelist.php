0357bc90363afb404ad2021b575496f4d3ce26a0
dev-djangotop1 <mressalehy@gmail.com>
2026-04-19 22:18:16 +0100

    Update mobilelist.php

--- mobilelist.php
+++ mobilelist.php
@@ -1,45 +1,82 @@
-ip 185.71.66.21:7772 Титаниум (Рекомендуется)
-ip 185.71.66.21:7775 Литиум
-ip 185.71.66.21:7774 Аурум
-ip 185.71.66.21:7773 Хромиум
-ip 185.71.66.21:7771 Платинум
-ipdm 185.71.66.21:7777 DM
-iptest 185.71.66.21:7111 Тестовый
-gtapart1 cache.zip
-gtapart1size 62074465
-gtapart1md5 11bf7ec9901266248a03805125bd7c74
-gtapart2 gta3.dxt.zip
-gtapart2size 127476654
-gtapart2md5 5920e34a9bda292870e294e08eb20dc3
-gtapart3 gta3.etc.zip
-gtapart3size 56445532
-gtapart3md5 cf97f23033b8b7e1a5646110f8a39f5c
-gtapart4 gta3.pvr.zip
-gtapart4size 83240385
-gtapart4md5 92b1a501baa530b9e81fdc350c79b5c6
-gtapart5 gta3.zip
-gtapart5size 117674234
-gtapart5md5 1b5d84d07533380a37b2a3a7db7763a9
-gtapart6 gta_int.zip
-gtapart6size 87909742
-gtapart6md5 86155c0b1af3e82d6ac881f7fc141e4c
-gtapart7 samp.zip
-gtapart7size 49913804
-gtapart7md5 a4e157700d619673fb230f15d4c12494
-gtapart8 textures.zip
-gtapart8size 54373589
-gtapart8md5 699b4ecd5865626f5641d70109a1c376
-gtapart9 textdrawaddon.zip
-gtapart9size 78651382
-gtapart9md5 6cc571dcc5ae1be66bf83f5301902784
-apk SAMPMobile.apk
-apksize 23318026
-apkmd5 16d1f0074b94d5e67f4e8ec3f7d8e65b
-apkversion 1
-apkversionname 1.171
-storeapk SAMPMobileStore.apk
-storeapksize 23093248
-storeapkmd5 801c275f5bfc465c1c3dfba07c42d6c0
-storeapkversion 7
-storeapkversionname 1.172
-Russia
+{
+  "servers": [
+    { "name": "Титаниум (Рекомендуется)", "ip": "185.71.66.21:7772" },
+    { "name": "Литиум", "ip": "185.71.66.21:7775" },
+    { "name": "Аурум", "ip": "185.71.66.21:7774" },
+    { "name": "Хромиум", "ip": "185.71.66.21:7773" },
+    { "name": "Платинум", "ip": "185.71.66.21:7771" },
+    { "name": "DM", "ip": "185.71.66.21:7777" },
+    { "name": "Тестовый", "ip": "185.71.66.21:7111" }
+  ],
+  "files": [
+    {
+      "name": "cache.zip",
+      "key": "gtapart1",
+      "size": 62074465,
+      "md5": "11bf7ec9901266248a03805125bd7c74"
+    },
+    {
+      "name": "gta3.dxt.zip",
+      "key": "gtapart2",
+      "size": 127476654,
+      "md5": "5920e34a9bda292870e294e08eb20dc3"
+    },
+    {
+      "name": "gta3.etc.zip",
+      "key": "gtapart3",
+      "size": 56445532,
+      "md5": "cf97f23033b8b7e1a5646110f8a39f5c"
+    },
+    {
+      "name": "gta3.pvr.zip",
+      "key": "gtapart4",
+      "size": 83240385,
+      "md5": "92b1a501baa530b9e81fdc350c79b5c6"
+    },
+    {
+      "name": "gta3.zip",
+      "key": "gtapart5",
+      "size": 117674234,
+      "md5": "1b5d84d07533380a37b2a3a7db7763a9"
+    },
+    {
+      "name": "gta_int.zip",
+      "key": "gtapart6",
+      "size": 87909742,
+      "md5": "86155c0b1af3e82d6ac881f7fc141e4c"
+    },
+    {
+      "name": "samp.zip",
+      "key": "gtapart7",
+      "size": 49913804,
+      "md5": "a4e157700d619673fb230f15d4c12494"
+    },
+    {
+      "name": "textures.zip",
+      "key": "gtapart8",
+      "size": 54373589,
+      "md5": "699b4ecd5865626f5641d70109a1c376"
+    },
+    {
+      "name": "textdrawaddon.zip",
+      "key": "gtapart9",
+      "size": 78651382,
+      "md5": "6cc571dcc5ae1be66bf83f5301902784"
+    }
+  ],
+  "apk": {
+    "name": "SAMPMobile.apk",
+    "size": 23318026,
+    "md5": "16d1f0074b94d5e67f4e8ec3f7d8e65b",
+    "version": 1,
+    "versionName": "1.171"
+  },
+  "store_apk": {
+    "name": "SAMPMobileStore.apk",
+    "size": 23093248,
+    "md5": "801c275f5bfc465c1c3dfba07c42d6c0",
+    "version": 7,
+    "versionName": "1.172"
+  },
+  "region": "Russia"
+}
